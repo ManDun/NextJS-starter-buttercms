ef838037d461087b178b079f247c5efd3d7e7554
Manas <manasm.dev@gmail.com>
2023-04-16 21:52:03 +1000

    updated style

--- components/header-section.js
+++ components/header-section.js
@@ -5,32 +5,39 @@ import Image from "next/image";
 import MainMenu from "./main-menu/main-menu";
 
 export default function HeaderSection({ mainMenu }) {
-    const [isNavbarSticky, setIsNavbarSticky] = useState(false);
-    const navbarAreaEl = useRef(null);
+  const [isNavbarSticky, setIsNavbarSticky] = useState(false);
+  const navbarAreaEl = useRef(null);
 
-    function fixNavBar() {
-        if (navbarAreaEl.current) {
-            setIsNavbarSticky(window.pageYOffset > navbarAreaEl.current.offsetTop)
-        }
+  function fixNavBar() {
+    if (navbarAreaEl.current) {
+      setIsNavbarSticky(window.pageYOffset > navbarAreaEl.current.offsetTop);
     }
+  }
 
-    useEffect(() => {
-        window.addEventListener('scroll', fixNavBar);
+  useEffect(() => {
+    window.addEventListener("scroll", fixNavBar);
 
-        return () => {
-            window.removeEventListener('scroll', fixNavBar);
-        }
-    }, []);
+    return () => {
+      window.removeEventListener("scroll", fixNavBar);
+    };
+  }, []);
 
-    return (
-        <header className="header">
-            <div ref={navbarAreaEl} className={`navbar-area ${isNavbarSticky ? 'sticky' : ''}`}>
-                <div className="container">
-                    <div className="row align-items-center">
-                        <div className="col-lg-12">
-                            <nav className="navbar navbar-expand-lg">
-                                <a className="navbar-brand" href="https://buttercms.com">
-                                    <Image
+  return (
+    <header className="header">
+      <div
+        ref={navbarAreaEl}
+        className={`navbar-area ${isNavbarSticky ? "sticky" : ""}`}
+      >
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-12">
+              <nav className="navbar navbar-expand-lg">
+                <a
+                  className="navbar-brand"
+                  href="https://buttercms.manasconsults.net"
+                >
+                  <h3>ManasBlogs</h3>
+                  {/* <Image
                                         src="https://cdn.buttercms.com/PBral0NQGmmFzV0uG7Q6"
                                         alt="Logo"
                                         width={180}
@@ -38,14 +45,14 @@ export default function HeaderSection({ mainMenu }) {
                                         style={{
                                             maxWidth: "100%",
                                             height: "auto"
-                                        }} />
-                                </a>
-                                <MainMenu mainMenuLinks={mainMenu} />
-                            </nav>
-                        </div>
-                    </div>
-                </div>
+                                        }} /> */}
+                </a>
+                <MainMenu mainMenuLinks={mainMenu} />
+              </nav>
             </div>
-        </header>
-    );
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
